bcdaafd68cb831ded7bdd02d40d6ebeb6ab83d67
000bachir <peakdev698@gmail.com>
2025-11-10 15:53:33 +0100

    supabase client added with more features

--- app/supabase/supabaseClient.py
+++ app/supabase/supabaseClient.py
@@ -15,11 +15,9 @@ from typing import Union
         2- check if the email is a valid email ==== done
         3- test the database with email ==== done
         4-  a function that will count how many rows i have ==== done
-        5- a function that will print only the email that i have with no count 
+        5- a function that will print only the email that i have with no count ==== done
         
 """
-
-
 #! env var keys
 url = loading_env_variables("PROJECT_URL") or ""
 key = loading_env_variables("ANON_PUBLIC_KEY") or ""
@@ -28,10 +26,10 @@ key = loading_env_variables("ANON_PUBLIC_KEY") or ""
 supabase_client: Client = create_client(url, key)
 
 # global timestamp
-timestamp = datetime.datetime.now(datetime.UTC).isoformat()
+# timestamp = datetime.datetime.now(datetime.UTC).isoformat()
+seeding_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
 
-# email record
 @dataclass
 class EmailRecord:
     email: str
@@ -45,95 +43,120 @@ class EmailRecord:
     notes: str = ""
 
 
-#! checking the health of the database
-def check_health(client: Client):
-    try:
-        client.table("healthcheck").insert(
-            {"status": "connection confirmed", "timestamp": timestamp}
-        ).execute()
-    except Exception as e:
-        client.table("healthcheck").insert(
-            {
-                "status": f"failed to check the database health of the connection cause : {e} ",
-                "timestamp": timestamp,
-            }
-        ).execute()
-        return False
+class DatabaseOperation:
+    def __init__(self):
+        self.client = supabase_client
 
+    """
+        checking the health of the database
+    """
 
-# * checking the connection
-def load_and_check_connection():
-    try:
-        response_check = (
-            supabase_client.table("healthcheck")
-            .select("*")
-            .order("timestamp", desc=True)
-            .limit(1)
-            .execute()
-        )
-        if response_check.data:
-            latest = response_check.data[0]
-            print(f"status: {latest['status']} at {latest['timestamp']}")
-            return True
-        return False
-    except Exception as e:
-        raise RuntimeError(f"check failed please see the error above: {e}")
+    def check_health(self):
+        seeding_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        try:
+            self.client.table("healthcheck").insert(
+                {"status": "connection confirmed", "timestamp": seeding_time}
+            ).execute()
+        except Exception as e:
+            self.client.table("healthcheck").insert(
+                {
+                    "status": f"failed to check the database health of the connection cause : {e} ",
+                    "timestamp": seeding_time,
+                }
+            ).execute()
+            return False
 
+    """
 
-def valid_email_pattern(email: str):
-    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    if re.fullmatch(pattern, email):
-        print("the email provided is valid")
-    else:
-        print("the email provided doesn't have a valid structure")
+    checking the connection
 
+    """
 
-def checking_for_dupalicates(record: EmailRecord):
-    try:
-        duplicate = (
-            supabase_client.table("email")
-            .select("email")
-            .eq("email", record.email)
-            .execute()
-        )
-        if duplicate.data:
-            print(
-                f"the email provided already exist in the database : {record.email} skipping"
+    def load_and_check_connection(self):
+        try:
+            response_check = (
+                self.client.table("healthcheck")
+                .select("*")
+                .order("timestamp", desc=True)
+                .limit(1)
+                .execute()
             )
-    except Exception as e:
-        raise RuntimeError(
-            f"failed checking for duplicate in the database please check the error above : {e}"
-        )
+            if response_check.data:
+                latest = response_check.data[0]
+                print(f"status: {latest['status']} at {latest['timestamp']}")
+                return True
+            return False
+        except Exception as e:
+            raise RuntimeError(f"check failed please see the error above: {e}")
 
+    """
+        valid email patterns
+    """
 
-def valid_record_needed(record: EmailRecord):
-    required_fields = {
-        "email": record.email,
-        "language": record.language,
-        "source": record.source,
-        "status": record.status,
-    }
-    for field_name, value in required_fields.items():
-        if not value:
-            raise ValueError(f"error the field {field_name} is required")
-    return True
-
-
-# seeding the database with test emails :
-def seeding_the_database(record: EmailRecord):
-    seeding = None
-    valid_email_pattern(record.email)
-    valid_record_needed(record)
-    # time of seeding :
-    try:
-        # normalizing timestamp
-        if isinstance(record.added_at, str):
-            record.added_at = datetime.datetime.now().isoformat()
-        elif isinstance(record.last_contacted_at, str):
-            record.last_contacted_at = record.added_at = (
-                datetime.datetime.now().isoformat()
-            )
+    def valid_email_pattern(self, email: str):
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if re.fullmatch(pattern, email):
+            print("the email provided is valid")
         else:
+            print("the email provided doesn't have a valid structure")
+
+    def checking_for_dupalicates(self, record: EmailRecord):
+        try:
+            duplicate = (
+                self.client.table("emails")
+                .select("email")
+                .eq("email", record.email)
+                .execute()
+            )
+            if duplicate.data:
+                print(
+                    f"the email provided already exist in the database : {record.email} skipping"
+                )
+                return True
+            return False
+        except Exception as e:
+            raise RuntimeError(
+                f"failed checking for duplicate in the database please check the error above : {e}"
+            )
+
+    """
+        valid email record
+    """
+
+    def valid_record_needed(self, record: EmailRecord):
+        required_fields = {
+            "email": record.email,
+            "language": record.language,
+            "source": record.source,
+            "status": record.status,
+        }
+        for field_name, value in required_fields.items():
+            if not value:
+                raise ValueError(f"error the field {field_name} is required")
+        return True
+
+    """
+        seeding the database with test emails 
+    """
+
+    def seeding_the_database(self, record: EmailRecord):
+        seeding = None
+        self.valid_email_pattern(record.email)  # check for valid email pattern
+        self.valid_record_needed(record)  # check if the required fields are available
+        duplicates = self.checking_for_dupalicates(record)
+        if duplicates:
+            print(f"email already in the database {record.email}")
+            return False
+        # time of seeding :
+        try:
+            print("the process of seeding the database is being initiated")
+            # normalizing timestamp
+            if isinstance(record.added_at, str):
+                record.added_at = datetime.datetime.now().isoformat()
+            if isinstance(record.last_contacted_at, str):
+                record.last_contacted_at = record.added_at = (
+                    datetime.datetime.now().isoformat()
+                )
             seeding = (
                 supabase_client.table("emails")
                 .insert(
@@ -151,70 +174,41 @@ def seeding_the_database(record: EmailRecord):
                 )
                 .execute()
             )
-        if seeding and seeding.data is not None:
-            print("the seeding of the database has been successful")
+            if seeding and seeding.data is not None:
+                print("the seeding of the database has been successful")
 
-    except Exception as e:
-        raise RuntimeError(
-            f"failed to seed the database with data please check the error : {e}"
-        )
+        except Exception as e:
+            raise RuntimeError(
+                f"failed to seed the database with data please check the error : {e}"
+            )
 
+    """
+        function that will count the number of rows in a database
+    """
 
-seeding_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-record = EmailRecord(
-    email=mock_email,
-    added_at=seeding_time,
-    last_contacted_at=seeding_time,
-    status="pending",
-    language="english",
-    notes="no note",
-    category="teaching",
-    full_name="bachir",
-    source="linkedin",
-)
-seeding_the_database(record)
+    def countRows(self):
+        try:
+            rows = self.client.table("emails").select("*", count="exact").execute()
+            print(f"the number of rows in the database are : {rows.count}")
+            if rows.count == 0:
+                print("the database has no rows inside of it")
+        except Exception as e:
+            raise RuntimeError(
+                f"error could not retreive how many rows are in the database for more info please check the error : {e}"
+            )
 
+    """
+        to fetch the emails stored in the database 
+    """
 
-def countRows():
-    try:
-        rows = supabase_client.table("emails").select("*", count="exact").execute()
-        print(f"the number of rows in the database are : {rows.count}")
-        if rows.count == 0:
-            print("the database has no rows inside of it")
-    except Exception as e:
-        raise RuntimeError(
-            f"error could not retreive how many rows are in the database for more info please check the error : {e}"
-        )
-
-
-def FetchEmails():
-    try:
-        fetch = supabase_client.table("emails").select("email").execute()
-        if fetch and fetch.data is None:
-            print("failed to fetch all of the email from the database")
-        else:
-            print(f"email fetched : {fetch.data}")
-    except Exception as e:
-        raise RuntimeError(
-            f"error operating the fetch request onto the database please check the error : {e}"
-        )
-
-
-FetchEmails()
-
-
-# if __name__ == "__main__":
-#     check_health(supabase_client)
-#     print("healthcheck : ", load_and_check_connection())
-#     print(
-#         "seeding the database",
-#         seeding_the_database(
-#             added_at=timestamp,
-#             last_contacted_at=timestamp,
-#             email=email_to_test,
-#             category="french",
-#             status="pending",
-#             source="facebook",
-#             notes="testing",
-#         ),
-#     )
+    def FetchEmails(self):
+        try:
+            fetch = self.client.table("emails").select("email").execute()
+            if fetch and fetch.data is None:
+                print("failed to fetch all of the email from the database")
+            else:
+                print(f"email fetched : {fetch.data}")
+        except Exception as e:
+            raise RuntimeError(
+                f"error operating the fetch request onto the database please check the error : {e}"
+            )
